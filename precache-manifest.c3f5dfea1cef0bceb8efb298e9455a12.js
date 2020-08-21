e9aa4e6370c7a48c4abc6144ee8ec59936783924
mex-dev <aderibigbeoluwaseun4@gmail.com>
2020-08-21 09:48:35 +0100

    Updates

---
+++ precache-manifest.c3f5dfea1cef0bceb8efb298e9455a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ca0a5d1c081039254806f8672e3530",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9f2c4c29f2b55e9670f9",
+    "url": "/Portfolio/static/css/main.c39711da.chunk.css"
+  },
+  {
+    "revision": "c2ca85f69643c2bbbbf4",
+    "url": "/Portfolio/static/js/2.5626cd74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.5626cd74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2c4c29f2b55e9670f9",
+    "url": "/Portfolio/static/js/main.97e4f10b.chunk.js"
+  },
+  {
+    "revision": "0f02361a6eac99662be5",
+    "url": "/Portfolio/static/js/runtime-main.52296364.js"
+  }
+]);
